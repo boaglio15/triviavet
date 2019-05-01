648b6428435f334c56d12ced9e48d3aca301030b
boaglio15 <agustinboaglio.95@gmail.com>
2019-05-01 20:25:58 -0300

    Eliminacion archivos obsoletos

--- src/main/java/trivia/App.java
+++ src/main/java/trivia/App.java
@@ -13,24 +13,46 @@ import trivia.User;
 
 import com.google.gson.Gson;
 import java.util.Map;
+import java.util.*;
 
 public class App
 {
     public static void main( String[] args )
     {
-      before((request, response) -> {
-        Base.open();
-      });
+      //before((request, response) -> {
+        //Base.open();
+      //});
+      Base.open("com.mysql.jdbc.Driver", "jdbc:mysql://localhost/trivia", "root", "root");
 
-      after((request, response) -> {
-        Base.close();
-      });
+
 
       get("/hello/:name", (req, res) -> {
           //System.out.println("hola mundo");
         return "hello" + req.params(":name");
+    });
+
+      /*post("/users", (req,res) -> {
+          User newUser = new User();
+          newUser.set("nom", "Agustin");
+          newUser.set("ape", "Boaglio");
+          newUser.set("dni", "37875774");
+          newUser.set("tipo", "ADMIN");
+          newUser.saveIt();
+
+          res.type("application/json");
+
+          return newUser.toJson(true);
+      });*/
+
+      get("/users", (req, res) -> {
+            res.type("application/json");
+            List<User> r = User.findAll();
+            return new Gson().toJson(r);
       });
 
+      //after((request, response) -> {
+        Base.close();
+      //});
       /*post("/users", (req, res) -> {
         Map<String, Object> bodyParams = new Gson().fromJson(req.body(), Map.class);
 
