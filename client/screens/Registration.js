55f36b648b5fa7795054f63cec45fdbded42f344
boaglio15 <agustinboaglio.95@gmail.com>
2019-10-02 18:41:26 -0300

    [#168849868] modificacion front-end en las screen singIn registration y home

--- client/screens/Registration.js
+++ client/screens/Registration.js
@@ -14,7 +14,7 @@ import axios from 'axios';
 
 export default class Registration extends React.Component {
     static navigationOptions = {
-        title: 'Registro',
+        header: null,
     };
 
     constructor(props) {
@@ -35,37 +35,46 @@ export default class Registration extends React.Component {
                     <Text style={styles.welcome}> Registrate! </Text>
 
                     <TextInput
-                        placeholder="Ingresa tu nombre"
+                        placeholder="Nombre"
                         style={styles.input}
                         onChangeText={(value) => this.setState({ nombre: value })}
                         value={this.state.nombre}
                     />
 
                     <TextInput
-                        placeholder="Ingresa tu apellido"
+                        placeholder="Apellido"
                         style={styles.input}
                         onChangeText={(value) => this.setState({ apellido: value })}
                         value={this.state.apellido}
                     />
 
                     <TextInput
-                        placeholder="Ingresa tu dni"
+                        placeholder="Dni"
                         style={styles.input}
                         onChangeText={(value) => this.setState({ dni: value })}
                         value={this.state.dni}
                     />
 
                     <TextInput
-                        placeholder="Ingresa tu password"
+                        placeholder="Password"
                         style={styles.input}
                         secureTextEntry={true}
                         onChangeText={(value) => this.setState({ pass: value })}
                         value={this.state.pass}
                     />
+                    <View style={styles.button}>
+                        <Button title="registrar!" onPress={this.registration} color="black"/>
+                    </View>
 
-                    <Button title="registrar!" onPress={this.registration} />
+                    <View style={styles.button}>
+                        <Button title="Volver" onPress={this.backLogin} color="black"/>
+                    </View>
                 </View>
         );
+    }
+
+    backLogin = () => {
+        this.props.navigation.navigate('SignIn')
     }
 
     registration = () => {
@@ -105,7 +114,9 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         justifyContent: 'center',
-        backgroundColor: '#F5FCFF',
+        backgroundColor: '#30A9AC',
+        alignItems:'center',
+        borderRadius: 500,
     },
     welcome: {
         fontSize: 40,
@@ -118,6 +129,11 @@ const styles = StyleSheet.create({
         padding: 5,
         fontSize: 16,
         borderBottomWidth: 3,
-        borderBottomColor: '#ff0000',
-    }
+        borderBottomColor: 'black',
+    },
+    button: {
+        marginTop:15,
+        paddingRight: 70,
+        paddingLeft: 70,
+  },
 })
